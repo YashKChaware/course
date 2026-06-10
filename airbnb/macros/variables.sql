d663e036370786c97a3b79cdc94217c52b588c97
YashKChaware <YashKiran.Chaware@cognizant.com>
2026-06-10 12:35:10 +0530

    set default value for dbt variable  in variables.sql

--- airbnb/macros/variables.sql
+++ airbnb/macros/variables.sql
@@ -3,6 +3,6 @@
     {% set your_name_jinja = "Yash" %} -- define jinja variable
     {{ log("Hello " ~ your_name_jinja, info=True) }} -- info = true for printing logs on console 
 
-    {{log("Hello dbt user " ~ var("user_name") ~ "!", info=True )}} -- use of dbt variable
+    {{log("Hello dbt user " ~ var("user_name", "No USERNAME  Is SET !!") ~ "!", info=True )}} -- use of dbt variable
 
 {% endmacro %}
