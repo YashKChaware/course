a928fa49ac637b6b26e2c8523a3a63495fb2e7f8
YashKChaware <YashKiran.Chaware@cognizant.com>
2026-06-10 12:21:00 +0530

    using DBT vaiable

--- airbnb/macros/variables.sql
+++ airbnb/macros/variables.sql
@@ -1,7 +1,8 @@
 {% macro learn_variables() %}
 
-    {% set your_name_jinja = "Yash" %}
-    {{ log("Hello " ~ your_name_jinja, info=True) }}
+    {% set your_name_jinja = "Yash" %} -- define jinja variable
+    {{ log("Hello " ~ your_name_jinja, info=True) }} -- info = true for printing logs on console 
 
+    {{log("Hello dbt user " ~ var("user_name") ~ "!", info=True )}} -- use of dbt variable
 
 {% endmacro %}
