fb8e23b5022c066a85c55226e19f5c38f7a78597
YashKChaware <YashKiran.Chaware@cognizant.com>
2026-06-11 16:48:12 +0530

    configured prod env to the project

--- airbnb/models/dim/dim_fullmoon.py
+++ airbnb/models/dim/dim_fullmoon.py
@@ -8,7 +8,7 @@ def is_holiday(date_col):
 def model(dbt, session):
     dbt.config(
         materialized = "table",
-        packages = ["holidays"]
+        packages = ["holidays"] ,
         enabled=False
     )
 
